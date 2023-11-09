44df8397e769469bf41588880093bbd2fcf96ba9
Maruf Mobin <mobinh264@gmail.com>
2023-11-09 22:03:12 +0600

    [ .env , config , install ts-node-dev , mongodb atlas , express , ] full setup

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import config from "./config";
+import app from './app'
+
+async function boostrap() {
+    try {
+        await mongoose.connect(config.database_url as string);
+        console.log(`Database Connected Successfully`)
+        app.listen(config.port, () => {
+            console.log(`Application Listening on port ${config.port}`)
+        })
+    }
+    catch (err) {
+        console.log('Failed To Connect Database', err);
+    }
+}
+boostrap();
