34eb0fb914f93508fcfeaa652ef3ac5f0017721e
Maruf Mobin <mobinh264@gmail.com>
2023-11-12 20:58:51 +0600

    Husky Added

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
-import express, { Application, Request, Response } from 'express';
+import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
 
-// use Express 
+// use Express
 const app: Application = express()
 
 // use Cors
@@ -11,9 +11,9 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-// Testing 
+// Testing
 app.get('/', (req: Request, res: Response) => {
-    res.send('Working Successfully')
+  res.send('Working Successfully')
 })
 
 export default app
--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,16 @@
-import mongoose from "mongoose";
-import config from "./config";
+import mongoose from 'mongoose'
+import config from './config'
 import app from './app'
 
 async function boostrap() {
-    try {
-        await mongoose.connect(config.database_url as string);
-        console.log(`Database Connected Successfully`)
-        app.listen(config.port, () => {
-            console.log(`Application Listening on port ${config.port}`)
-        })
-    }
-    catch (err) {
-        console.log('Failed To Connect Database', err);
-    }
+  try {
+    await mongoose.connect(config.database_url as string)
+    console.log(`Database Connected Successfully`)
+    app.listen(config.port, () => {
+      console.log(`Application Listening on port ${config.port}`)
+    })
+  } catch (err) {
+    console.log('Failed To Connect Database', err)
+  }
 }
-boostrap();
+boostrap()
