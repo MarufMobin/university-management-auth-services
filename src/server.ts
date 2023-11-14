8a04e4de6ea7355924676cc2df3e6d6f0782785c
Maruf Mobin <mobinh264@gmail.com>
2023-11-15 00:59:21 +0600

    Winston Logger Added in our Application

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,17 @@
 import mongoose from 'mongoose'
-import config from './config'
 import app from './app'
+import config from './config'
+import { errorLogger, logger } from './shared/logger'
 
 async function boostrap() {
   try {
     await mongoose.connect(config.database_url as string)
-    console.log(`Database Connected Successfully`)
+    logger.info(`Database Connected Successfully`)
     app.listen(config.port, () => {
-      console.log(`Application Listening on port ${config.port}`)
+      logger.info(`Application Listening on port ${config.port}`)
     })
   } catch (err) {
-    console.log('Failed To Connect Database', err)
+    errorLogger.error('Failed To Connect Database', err)
   }
 }
 boostrap()
